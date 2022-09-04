e6b696a6cfcc95a14f2e99e3c939e04826b41720
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-04 12:22:37 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,377 +1,210 @@
-let userName = document.getElementById('name');
-userName.focus();
+// The Name field
+window.onload = function () {
+    document.getElementById('name').focus();
+};
 
-const nameField = document.querySelector('#name');
-const otherJobField = document.querySelector('#other-job-role');
-const jobRoleSelect = document.querySelector('#title');
+//Job Role
+const otherJobRole = document.getElementById('other-job-role');
+otherJobRole.style.display = 'none';
 
+const jobTitle = document.getElementById('title');
+jobTitle.addEventListener('change', e => {
+    (e.target.value === 'other')? otherJobRole.style.display = '' : otherJobRole.style.display = 'none';
+});
 
+// T-Shirt Info
+const designTheme = document.getElementById('design');
+const colorTheme = document.getElementById('color');
+const colorThemeOption = colorTheme.children;
+colorTheme.disabled = true;
 
-//  job role selector
-otherJobField.style.display = 'none';
-jobRoleSelect.addEventListener('change', (e) => {
-    const selected = e.target.value;
-    if (selected === 'other') {
-        otherJobField.style.removeProperty('display');
+designTheme.addEventListener ('change', e=> {
+    colorTheme.disabled = false;
+    for (let i=0; i < colorThemeOption.length; i++){
+        const selectTheme = e.target.value;
+        const themeOption = colorThemeOption[i].getAttribute('data-theme');
+        if (selectTheme === themeOption) {
+            colorThemeOption[i].hidden = false;
+            colorThemeOption[i].setAttribute('selected', true);
+        } else {
+            colorThemeOption[i].hidden = true;
+            colorThemeOption[i].removeAttribute('selected');
+        }
+    }
+}); 
+
+// Register for Activities
+const activitiesSelected = document.getElementById('activities-box');
+const activitiesAmount = document.getElementById('activities-cost');
+const allSelected = document.querySelectorAll('input[type="checkbox"]');
+let allCosts = 0;
+
+activitiesSelected.addEventListener('change', e => {
+    const costSelected = parseInt(e.target.getAttribute('data-cost'));
+    (e.target.checked)? allCosts += costSelected : allCosts -= costSelected;
+    activitiesAmount.textContent = `Total: $${allCosts}`;
+
+    const dayTime = e.target.getAttribute('data-day-and-time');
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        for (let i=0; i < allSelected.length; i++) {
+            if (e.target !== allSelected[i] && dayTime === allSelected[i].getAttribute('data-day-and-time')) {
+                allSelected[i].disabled = true;
+                allSelected[i].parentElement.classList.add('disabled');
+            }
+        }
     } else {
-        otherJobField.style.display = 'none';
-    }
-});
-
-const shirtDesignSelect = document.querySelector('#design');
-const shirtColorSelect = document.querySelector('#color');
-const jsPunsColors = document.querySelectorAll('[data-theme="js puns"]');
-const heartJsColors = document.querySelectorAll('[data-theme="heart js"]');
-
-// shirt color options are disabled until a design option is selected
-shirtColorSelect.disabled = true;
-
-//  this adds/removes hidden attribute to shirt colors
-shirtDesignSelect.addEventListener('change', (e) => {
-    const selected = e.target.value;
-    const jsPunsOption = shirtDesignSelect[1];
-    shirtColorSelect.disabled = false;
-
-    for (let i = 0; i < 3; i++) {
-        if (selected === jsPunsOption.value) {
-            shirtColorSelect[0].selected = true;
-            heartJsColors[i].hidden = true;
-            jsPunsColors[i].hidden = false;
-        } else {
-            shirtColorSelect[0].selected = true;
-            jsPunsColors[i].hidden = true;
-            heartJsColors[i].hidden = false;
+        for (let i=0; i < allSelected.length; i++) {
+            if (dayTime === allSelected[i].getAttribute('data-day-and-time')) {
+                allSelected[i].disabled = false; 
+                allSelected[i].parentElement.classList.remove('disabled');
+            }
         }
     }
-});
+}); 
 
-const activities = document.querySelectorAll('#activities input');
-const activitiesCost = document.querySelector('#activities-cost');
-const activityFieldset = document.querySelector('#activities');
-let totalCost = 0;
+// Payment Info
+const paymentMethods = document.getElementById('payment');
+paymentMethods.children[1].setAttribute('selected', true);
 
-// change occurs for loop adds cost of activity to total price if checked
+const creditCard = document.getElementById('credit-card');
+creditCard.style.display = '';
 
-activityFieldset.addEventListener('change', (e) => {
-    const checked = e.target;
-    const checkedPrice = checked.getAttribute('data-cost');
-    const checkedDayTime = checked.getAttribute('data-day-and-time');
+const bitcoin = document.getElementById('bitcoin');
+bitcoin.style.display = 'none';
 
+const paypal = document.getElementById('paypal');
+paypal.style.display = 'none';
 
-    if (checked.checked) {
-        totalCost += parseInt(checkedPrice);
+paymentMethods.addEventListener ('change', e => {
+    const selectPaymentMethod = e.target.value;
+    switch (selectPaymentMethod) {
+        case 'paypal':
+            paypal.style.display = '';
+            bitcoin.style.display = 'none';
+            creditCard.style.display = 'none';
+            break;
+        case 'bitcoin':
+            bitcoin.style.display = '';
+            creditCard.style.display = 'none';
+            paypal.style.display = 'none';
+            break;
+        case 'creditCard':
+            creditCard.style.display = '';
+            paypal.style.display = 'none';
+            bitcoin.style.display = 'none';
+            break;
+        default:
+            creditCard.style.display = '';
+            paypal.style.display = 'none';
+            bitcoin.style.display = 'none';    
+    }
+}); 
+
+// Form validation
+// Form inputs
+const form = document.querySelector("form");
+const nameValid = document.getElementById("name");
+const email = document.getElementById("email");
+const ccNumber = document.getElementById("cc-num");
+const zip = document.getElementById("zip");
+const cvv = document.getElementById("cvv");
+
+// Validating name field
+function isValidName() {
+    const validCheck = /^\s*$/.test(nameValid.value);
+    (!validCheck)? noRequiredHint(nameValid) : requiredHint(nameValid);
+    return validCheck;
+};
+
+nameValid.addEventListener ('keyup', isValidName);
+
+// Validate email field
+function isEmailValid() {
+    const isEmpty = /^\s*$/.test(email.value);
+    const isValid = /^[^@]+\@[^@.]+\.com$/i.test(email.value);
+
+    if (isEmpty) {
+        requiredHint(email);
+        email.parentElement.lastElementChild.innerHTML = 'Email field cannot be blank';
+        return false;
+    } else if (!isValid) {
+        requiredHint(email);
+        email.parentElement.lastElementChild.innerHTML = 'Email address must contain @ sign and end with ".com"';
+        return false;
     } else {
-        totalCost -= parseInt(checkedPrice);
+        noRequiredHint(email);
+        return true;
     }
-
-    activitiesCost.innerHTML = `Total: $${totalCost}`;
-
-    // when checked the activity time & day matches other activities. it disable conflicting activities
-    for (let i = 0; i < activities.length; i++) {
-        const activity = activities[i];
-        const activityDayTime = activity.getAttribute('data-day-and-time')
-        if (checkedDayTime === activityDayTime && activity !== checked && checked.checked === true) {
-            activity.disabled = true;
-            activity.parentElement.classList.add('disabled');
-        } else if (checkedDayTime === activityDayTime && activity !== checked && checked.checked === false) {
-            activity.disabled = false;
-            activity.parentElement.classList.remove('disabled');
-        }
-    }
-});
-
-const paymentSelect = document.querySelector('#payment');
-const paymentMethods = document.querySelector('.payment-methods').children
-const creditCard = document.querySelector('#credit-card');
-const payPal = document.querySelector('#paypal');
-const bitCoin = document.querySelector('#bitcoin');
-
-// starts  on credit card
-paymentSelect[1].selected = true;
-creditCard.hidden = false;
-payPal.hidden = true;
-bitCoin.hidden = true;
-
-// when user selects the payment menthod it shows only the selected one and others are hidden
-paymentSelect.addEventListener('change', (e) => {
-    const selected = e.target.value;
-    for (let i = 0; i < paymentMethods.length; i++) {
-
-        if (selected === 'credit-card') {
-            creditCard.hidden = false;
-            paymentMethods[3].hidden = true;
-            paymentMethods[4].hidden = true;
-        }
-        if (selected === 'paypal') {
-            payPal.hidden = false;
-            paymentMethods[2].hidden = true;
-            paymentMethods[4].hidden = true;
-        }
-        if (selected === 'bitcoin') {
-            bitCoin.hidden = false;
-            paymentMethods[2].hidden = true;
-            paymentMethods[3].hidden = true;
-        }
-    }
-});
-
-function nameValidator() {
-    const nameValue = nameField.value;
-    // regex provided by teamtreehouse interactive form input validation course
-    const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue) && nameValue !== '';
-    return nameIsValid
-}
-
-const emailField = document.querySelector('#email');
-
-function emailValidator() {
-    const emailValue = emailField.value;
-    // regex provided by teamtreehouse interactive form input validation course
-    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue) && emailValue !== '';
-    return emailIsValid;
-}
-
-function activityValidator() {
-    let cost = activitiesCost.innerText;
-    // regex to remove nondigits provided by Alex Wayne on stackoverflow
-    cost = parseInt(cost.replace(/\D/g, ''));
-    const activityIsValid = cost > 0;
-    return activityIsValid;
-}
-
-const ccNumberField = document.querySelector('#cc-num');
-
-function ccNumberValidator() {
-    const ccNumberValue = ccNumberField.value;
-    const ccNumberIsValid = /^\d{13,16}$/g.test(ccNumberValue) && ccNumberValue !== '';
-    return ccNumberIsValid;
-}
-
-const zipCodeField = document.querySelector('#zip');
-
-function zipCodeValidator() {
-    const zipCodeValue = zipCodeField.value;
-    const isZipCodeValid = /^\d{5}$/g.test(zipCodeValue) && zipCodeValue !== '';
-    return isZipCodeValid;
-}
-
-const cvvField = document.querySelector('#cvv');
-
-function cvvValidator() {
-    const cvvValue = cvvField.value;
-    const isCvvValid = /^\d{3}$/g.test(cvvValue) && cvvValue !== '';
-    return isCvvValid;
-}
-
-/**
- * This function changes the styling of invalid fields.
- * 
- * @param {variable} field variable containing the field you want to style as invalid
- */
-function invalidField(field) {
-    field.parentElement.classList.add('not-valid');
-    field.parentElement.classList.remove('valid');
-    field.parentElement.lastElementChild.classList.remove('hint');
-}
-
-/**
- * This function changes the styling of invalid activity fieldset.
- * 
- * @param {variable} fieldset variable containing the fieldset you want to style as invalid
- */
-function invalidActivityField(fieldset) {
-    fieldset.classList.add('not-valid');
-    fieldset.classList.remove('valid');
-    fieldset.lastElementChild.classList.remove('hint');
-}
-
-// This function changes the styling of valid fields.
- 
- 
-function validField(field) {
-    field.parentElement.classList.add('valid');
-    field.parentElement.classList.remove('not-valid');
-    field.parentElement.lastElementChild.classList.add('hint');
-}
+};
 
 
- 
-function validActivityField(fieldset) {
-    fieldset.parentElement.classList.add('valid');
-    fieldset.parentElement.classList.remove('not-valid');
-    fieldset.lastElementChild.classList.add('hint');
-}
 
-// prevents form submission and puts error message for invalid field
-// shows checkmark by field if valid
-const form = document.querySelector('form');
+function isActivitySelected() {
+    const valid = allCosts > 0;
+    (valid)? noRequiredHint(activitiesSelected) : requiredHint(activitiesSelected);
+    return valid; 
+};
 
-form.addEventListener('submit', (e) => {
 
-    if (!nameValidator()) {
+function isCreditCardValid() {
+    const valid = /^(\d{13,16})$/.test(ccNumber.value);
+    (valid)? noRequiredHint(ccNumber) : requiredHint(ccNumber);
+    return valid; 
+};
+
+
+function isZipValid() {
+    const valid = /^(\d{5})$/.test(zip.value);
+    (valid)? noRequiredHint(zip) : requiredHint(zip);
+    return valid; 
+};
+
+
+function isCvvValid() {
+    const valid = /^(\d{3})$/.test(cvv.value);
+    (valid)? noRequiredHint(cvv) : requiredHint(cvv);
+    return valid; 
+};
+
+// Validate form
+form.addEventListener('submit', e => {
+    if (isValidName()) {
         e.preventDefault();
-        invalidField(nameField);
-        if (nameField.value === '') {
-            nameField.parentElement.lastElementChild.innerText = 'Name field cannot be blank.'
-        } else {
-            nameField.parentElement.lastElementChild.innerText = 'Name must only contain alphabetical characters.'
-        }
     }
-
-    if (nameValidator()) {
-        validField(nameField);
-    }
-
-    if (!emailValidator()) {
+    if (!isEmailValid()) {
         e.preventDefault();
-        invalidField(emailField);
-        if (emailField.value === '') {
-            emailField.parentElement.lastElementChild.innerText = 'Email field cannot be blank.'
-        } else {
-            emailField.parentElement.lastElementChild.innerText = 'Email address must be formatted properly.'
-        }
     }
-
-    if (emailValidator()) {
-        validField(emailField);
-    }
-
-    if (!activityValidator()) {
+    if (!isActivitySelected()) {
         e.preventDefault();
-        invalidActivityField(activityFieldset);
     }
-
-    if (activityValidator()) {
-        validActivityField(activityFieldset);
-    }
-
-    if (paymentSelect[1].selected) {
-        if (!ccNumberValidator()) {
-            e.preventDefault();
-            invalidField(ccNumberField);
-        }
-
-        if (ccNumberValidator()) {
-            validField(ccNumberField);
-        }
-    }
-
-    if (!zipCodeValidator()) {
+    if (creditCard.style.display === '' && !isCreditCardValid()) {
         e.preventDefault();
-        invalidField(zipCodeField);
     }
-
-    if (zipCodeValidator()) {
-        validField(zipCodeField);
-    }
-
-    if (!cvvValidator()) {
+    if (creditCard.style.display === '' && !isZipValid()) {
         e.preventDefault();
-        invalidField(cvvField);
     }
-
-    if (cvvValidator()) {
-        validField(cvvField);
+    if (creditCard.style.display === '' && !isCvvValid()) {
+        e.preventDefault();
     }
 });
 
-// provides real time feedback to user for errors, conditional error messaging for name & email fields
-nameField.addEventListener('keyup', () => {
-    if (!nameValidator()) {
-        invalidField(nameField);
-        if (nameField.value === '') {
-            nameField.parentElement.lastElementChild.innerText = 'Name field cannot be blank.'
-        } else {
-            nameField.parentElement.lastElementChild.innerText = 'Name must only contain alphabetical characters.'
-        }
-    }
-
-    if (nameValidator()) {
-        validField(nameField);
-    }
-});
-
-emailField.addEventListener('keyup', () => {
-    if (!emailValidator()) {
-        invalidField(emailField);
-        if (emailField.value === '') {
-            emailField.parentElement.lastElementChild.innerText = 'Email field cannot be blank.'
-        } else {
-            emailField.parentElement.lastElementChild.innerText = 'Email address must be formatted properly.'
-        }
-    }
-
-    if (emailValidator()) {
-        validField(emailField);
-    }
-});
-
-activityFieldset.addEventListener('change', () => {
-    if (!activityValidator()) {
-        invalidActivityField(activityFieldset);
-    }
-
-    if (activityValidator()) {
-        validActivityField(activityFieldset);
-    }
-});
-
-ccNumberField.addEventListener('keyup', () => {
-    if (!ccNumberValidator()) {
-        invalidField(ccNumberField);
-    }
-
-    if (ccNumberValidator()) {
-        validField(ccNumberField);
-    }
-});
-
-zipCodeField.addEventListener('keyup', () => {
-    if (!zipCodeValidator()) {
-        invalidField(zipCodeField);
-    }
-
-    if (zipCodeValidator()) {
-        validField(zipCodeField);
-    }
-});
-
-cvvField.addEventListener('keyup', () => {
-    if (!cvvValidator()) {
-        invalidField(cvvField);
-    }
-
-    if (cvvValidator()) {
-        validField(cvvField);
-    }
-});
-// accesbility 
-function validationPass(element) {
-    let parameterParent = element.parentElement;
-	parameterParent.classList.add('valid');
-	parameterParent.classList.remove('not-valid');
-	element.nextElementSibling.style.display = 'none';
+// Accessibility 
+for (let i=0; i < allSelected.length; i++) {
+    allSelected[i].addEventListener('focus', e => {e.target.parentElement.classList.add('focus')});
+    allSelected[i].addEventListener('blur', e => {e.target.parentElement.classList.remove('focus')});
 }
 
-function validationFail(element) {
-    let parameterParent = element.parentElement;
-	parameterParent.classList.add('not-valid');
-	parameterParent.classList.remove('valid');
-	element.nextElementSibling.style.display = 'block';
-}
+// Hint validation
+function requiredHint (element) {
+    element.parentElement.classList.add('not-valid');
+    element.parentElement.classList.remove('valid');
+    element.parentElement.lastElementChild.style.display = 'block';
+};
 
-
-// adds  obvious focus indicator to activities when tabbing through
-for (let i = 0; i < activities.length; i++) {
-    const activityLabels = document.querySelectorAll('#activities label');
-    const activity = activities[i];
-    const label = activityLabels[i];
-
-    activity.addEventListener('focus', () => {
-        label.classList.add('focus');
-    });
-
-    activity.addEventListener('blur', () => {
-        label.classList.remove('focus');
-    });
-}
+function noRequiredHint (element) {
+    element.parentElement.classList.add('valid');
+    element.parentElement.classList.remove('not-valid');
+    element.parentElement.lastElementChild.style.display = "none";
+};
